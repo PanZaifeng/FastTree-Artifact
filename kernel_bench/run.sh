e93dbd71fa041464415b8dfb79087710a84e9791
PanZaifeng <panzf1999@gmail.com>
2025-03-10 19:54:18 +0000

    force run

--- kernel_bench/run.sh
+++ kernel_bench/run.sh
@@ -5,6 +5,6 @@ kernel_bench_dir=$(dirname "$0")
 
 docker build -f ${kernel_bench_dir}/Dockerfile -t fasttree_kernel:latest ${kernel_bench_dir}
 docker run --rm -v ${kernel_bench_dir}:/kernel_bench --gpus all -it fasttree_kernel:latest sh -c \
-    "cd /kernel_bench && ./run_benchmark.sh \
+    "cd /kernel_bench && ./run_benchmark.sh true \
         && python ./parse_output.py > parsed_output.md \
         && python ./plot_norm_perf.py"
